38c5e9076c049cc9cdb2be02168cd85ca7c3c1d6
Naravich Chutisilp <naravich@mit.edu>
2024-06-06 09:31:55 -0400

    fix: same port in use

--- mmseg/train_fold0_learnable_encoder.sh
+++ mmseg/train_fold0_learnable_encoder.sh
@@ -4,5 +4,6 @@
 source activate openmmlab
 
 FOLD=0
+RANDOM_PORT=$((RANDOM % 64512 + 1024))
 
-python -m torch.distributed.launch --nnodes=1 --nproc_per_node=1 train.py configs/radio/radio_linear_8xb2-80k_voc-512x512_learnable.py --launcher pytorch --cfg-options "val_dataloader.dataset.ann_file=ImageSets/val_${FOLD}.txt" --cfg-options "train_dataloader.dataset.ann_file=ImageSets/train_${FOLD}.txt" --cfg-options "test_dataloader.dataset.ann_file=ImageSets/test.txt" --work-dir work_dirs/radio_linear_8xb2-80k_voc-512x512_fold${FOLD}_learnable_encoder --cfg-options "test_evaluator.output_dir=work_dirs/test_fold${FOLD}" --amp
+python -m torch.distributed.launch --nnodes=1 --nproc_per_node=1 --master_port=$RANDOM_PORT train.py configs/radio/radio_linear_8xb2-80k_voc-512x512_learnable.py --launcher pytorch --cfg-options "val_dataloader.dataset.ann_file=ImageSets/val_${FOLD}.txt" --cfg-options "train_dataloader.dataset.ann_file=ImageSets/train_${FOLD}.txt" --cfg-options "test_dataloader.dataset.ann_file=ImageSets/test.txt" --work-dir work_dirs/radio_linear_8xb2-80k_voc-512x512_fold${FOLD}_learnable_encoder --cfg-options "test_evaluator.output_dir=work_dirs/test_fold${FOLD}" --amp
